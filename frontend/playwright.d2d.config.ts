6d2a88bf9a8d2f08993ca91e945a3784b5adae66
Joep <145749778+jschuurk-kr@users.noreply.github.com>
2024-08-09 17:29:03 +0200

    WIP: updates d2d config

--- frontend/playwright.d2d.config.ts
+++ frontend/playwright.d2d.config.ts
@@ -14,8 +14,25 @@ const config: PlaywrightTestConfig = defineConfig({
   webServer: [
     {
       // TODO: change as part of #218 (run in pipeline) after #154 (simpler loading of fixtures)
+
+      // local:
+      // goal: run tests and run app against same db but on different port
+      // requires: npm run build and cargo build and ../backend/target/debug/api --reset-database --seed-data
+
+      // so no reset db and seed data
+      // no npm run build because takes too long
+      // so cargo run or running from target/debug/api
+      // ../backend/target/debug/api --frontend-dist ./dist
+
+      // pipeline: build frontend, build backend, run frontend
+      // goal: reliable state to run tests against
+
+      // this puts the db.sqlite in frontend, so nope
       command:
-        "cd ../backend && cargo run --bin api -- --frontend-dist ../frontend/dist --port 8081",
+        "cd ../backend/target/debug && ./api --frontend-dist ../../../frontend/dist --port 8081",
+      // process.env.CI ?
+      //   "cd ../backend && cargo run -- --reset-database --seed-data --frontend-dist ../frontend/dist --port 8081" :
+      //   "../backend/target/debug/api --frontend-dist dist",
       port: 8081,
     },
   ],
